4d98ab9c6b3df8e18fc2227713c0646bef07e138
ichirotiheiroki <74300287+ichirotiheiroki@users.noreply.github.com>
2022-07-31 14:03:13 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,169 @@
-const form = document.querySelector("#form"), taskInput = document.querySelector("#taskInput"), tasksList = document.querySelector("#tasksList"), emptyList = document.querySelector("#emptyList"); let tasks = []; function addTask(b) { b.preventDefault(); let c = taskInput.value, a = { id: Date.now(), text: c, done: !1 }; tasks.push(a), saveLocaleStorage(), renderTask(a), taskInput.value = "", taskInput.focus(), checkEmptyList() } function deleteTask(a) { if ("delete" !== a.target.dataset.action) return; let b = a.target.closest("li"), d = Number(b.id), c = tasks.findIndex(function (a) { return a.id === d }); tasks.splice(c, 1), saveLocaleStorage(), b.remove(), checkEmptyList() } function doneTask(b) { if ("done" === b.target.dataset.action) { let a = b.target.closest("li"), e = a.id, c = tasks.find(function (a) { if (a.id === +e) return !0 }); c.done = !c.done, saveLocaleStorage(); let d = a.querySelector("span"); d.classList.toggle("task-title--done"), a.style.backgroundColor.toggle = "#d1e7b8" } } function checkEmptyList() { if (0 === tasks.length) { let b = `<li id="emptyList" class="list-group-item empty-list"><img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3"><div class="empty-list__title">Список дел пуст</div></li>`; tasksList.insertAdjacentHTML("afterbegin", b) } if (tasks.length > 0) { let a = document.querySelector("#emptyList"); a && a.remove() } } function saveLocaleStorage() { localStorage.setItem("tasks", JSON.stringify(tasks)) } function renderTask(a) { let b = a.done ? "task-title task-title--done" : "task-title", c = `<li id="${a.id}" class="list-group-item d-flex justify-content-between task-item"><span class="${b}">${a.text}</span><div class="task-item__buttons"><button type="button" data-action="done" class="btn-action"><img src="./img/tick.svg" alt="Done" width="18" height="18"></button><button type="button" data-action="delete" class="btn-action"><img src="./img/cross.svg" alt="Done" width="18" height="18"></button></div></li>`; tasksList.insertAdjacentHTML("beforeend", c) } localStorage.getItem("tasks") && (tasks = JSON.parse(localStorage.getItem("tasks"))).forEach(function (a) { renderTask(a) }), checkEmptyList(), form.addEventListener("submit", addTask), tasksList.addEventListener("click", deleteTask), tasksList.addEventListener("click", doneTask)
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
+let tasks = [];
+if (localStorage.getItem('tasks')) {
+	tasks = JSON.parse(localStorage.getItem('tasks'));
+	tasks.forEach(function (task) {
+		renderTask(task);
+	})
+}
+
+checkEmptyList()
+form.addEventListener('submit', addTask)
+tasksList.addEventListener('click', deleteTask)
+tasksList.addEventListener('click', doneTask)
+
+function addTask(e) {
+
+	e.preventDefault();
+	const taskText = taskInput.value;
+
+	const newTask = {
+		id: Date.now(),
+		text: taskText,
+		done: false,
+	};
+	tasks.push(newTask);
+	saveLocaleStorage();
+	renderTask(newTask);
+	taskInput.value = "";
+	taskInput.focus();
+	checkEmptyList()
+}
+
+function deleteTask(e) {
+	if (e.target.dataset.action !== 'delete') return;
+	const parentNode = e.target.closest('li');
+	const id = Number(parentNode.id);
+	const index = tasks.findIndex(function (task) {
+		return task.id === id;
+	});
+	tasks.splice(index, 1);
+	saveLocaleStorage();
+	parentNode.remove();
+	checkEmptyList()
+}
+
+function doneTask(e) {
+	if (e.target.dataset.action === "done") {
+		const parentNode = e.target.closest('li');
+		const id = parentNode.id;
+		const task = tasks.find(function (task) {
+			if (task.id === +id) {
+				return true
+			}
+		})
+		task.done = !task.done
+		saveLocaleStorage();
+		const span = parentNode.querySelector('span');
+		span.classList.toggle('task-title--done');
+		parentNode.classList.toggle('bg-color--done');
+	}
+}
+
+function checkEmptyList() {
+	if (tasks.length === 0) {
+		const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+											<img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+											<div class="empty-list__title">Список дел пуст</div>
+										</li>`;
+		tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+	}
+	if (tasks.length > 0) {
+		const emptyListEL = document.querySelector('#emptyList');
+		emptyListEL ? emptyListEL.remove() : null;
+	}
+}
+
+function saveLocaleStorage() {
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function renderTask(task) {
+	const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+	const bgColor = task.done ? 'bg-color--done' : '';
+	const taskHTML =
+		`<li id="${task.id}" class="${bgColor} list-group-item d-flex justify-content-between task-item">
+		<span class="${cssClass}">${task.text}</span>
+		<div class="task-item__buttons">
+			<button type="button" data-action="done" class="btn-action">
+				<img src="./img/tick.svg" alt="Done" width="18" height="18">
+			</button>
+			<button type="button" data-action="delete" class="btn-action">
+				<img src="./img/cross.svg" alt="Done" width="18" height="18">
+			</button>
+		</div>
+		</li>`;
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);
+}
+
+
+
+
+
+
+
+
+
+
+setInterval(function () {
+
+	var d = new Date();
+
+	document.getElementById('Day').innerHTML =
+		day(d.getDay());
+
+	document.getElementById('Date').innerHTML =
+		month(d.getMonth()) + ' ' +
+		d.getDate() + " " +
+		d.getFullYear();
+
+	document.getElementById('Time').innerHTML =
+		time(d.getHours()) + ':' +
+		place(d.getMinutes()) + ':' +
+		place(d.getSeconds()) + ' ' +
+		am(d.getHours());
+
+	function day(n) {
+		let days = ['Воскресенье',
+			'Понедельник',
+			'Вторник',
+			'Среда',
+			'Четверг',
+			'Пятница',
+			'Суббота']
+		return days[n]
+	}
+
+	function month(n) {
+		let months = ['Январь',
+			'Фквраль',
+			'Март',
+			'Апрель',
+			'Май',
+			'Июнь',
+			'Июль',
+			'Август',
+			'Сентябрь',
+			'Октябрь',
+			'Ноябрь',
+			'Декабрь'];
+		return months[n];
+	}
+
+	function time(n) {
+		n = n % 12
+		return n == 0 ? 12 : n
+	}
+
+	function place(n) {
+		return n < 10 ? '0' + n : n
+	}
+
+	function am(n) {
+		return (n > 12) ? "pm" : "am";
+	}
+
+}, 1000)
